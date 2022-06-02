592d1cc22f99640d34d6d34ceb0586d3744f6b6d
Mehak Gupta <96685817+MehakGupta1103@users.noreply.github.com>
2022-06-02 20:59:24 +0530

    tree questions

---
+++ tree & graph/LCA.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+vector<int> graph[N];
+
+int par[N];
+
+void dfs(int vertex, int p = -1)
+{
+    par[vertex] = p;
+    for (auto child : graph[vertex])
+    {
+        if (child == p)
+            continue;
+        dfs(child, vertex);
+    }
+}
+
+vector<int> path(int v)
+{
+    vector<int> ans;
+    while (v != -1)
+    {
+        ans.push_back(v);
+        v = par[v];
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+{
+    int node, edge;
+    cin >> node >> edge;
+    int n1, n2;
+    for (int i = 0; i < edge; i++)
+    {
+        cin >> n1 >> n2;
+        graph[n1].push_back(n2);
+        graph[n2].push_back(n1);
+    }
+    dfs(1);
+
+    int x, y;
+    cin >> x >> y;
+    vector<int> path_x = path(x);
+    vector<int> path_y = path(y);
+    int lca = -1;
+    int minLen = min(path_x.size(), path_y.size());
+    for(int i = 0; i < minLen; i++){
+        if(path_x[i] == path_y[i]) lca = path_x[i];
+        else break;
+    }
+    // if(path_x[0] == path_y[0]) lca = path_x[0];
+    cout << lca << "  the least common ansistor is " << endl;
+    return 0;
+}
---
+++ tree & graph/multisourceSP.cpp
@@ -0,0 +1,96 @@
+// multisource shortest path
+// snakes and capitalism to sochiallism --> codecheff
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e3 + 10;
+const int INF = 1e9 + 10;
+int val[N][N];
+int vis[N][N];
+int lev[N][N];
+int n, m;
+
+vector<pair<int, int>> movemens = {
+    {0,1}, {0, -1}, {1, 0}, {-1, 0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}
+};
+
+bool isvalid(int i, int j){
+    return i>=0 && j>=0 && i<n && j<n;
+}
+
+int bfs()
+{
+    int mx = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            mx = max(mx, val[i][j]);
+        }
+    }
+    queue<pair<int, int>> q;
+    for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(mx == val[i][j]){
+                    q.push({i, j});
+                    lev[i][j] = 0;
+                    vis[i][j] = 1;
+                }
+            }
+        }
+        int ans = 0;
+        while (!q.empty())
+        {
+            auto v = q.front();
+            int v_x = v.first;
+            int v_y = v.second;
+            q.pop();
+            for(auto movement : movemens){
+                int child_x = movement.first + v_x;
+                int child_y = movement.second + v_y;
+                if(!isvalid(child_x, child_y)) continue;
+                if(vis[child_x][child_y]) continue;
+                q.push({child_x, child_y});
+                lev[child_x][child_y] = lev[v_x][v_y] + 1;
+                vis[child_x][child_y] = 1;
+                ans = max(ans, lev[child_x][child_y]);
+            }
+        }
+        return ans;
+}
+
+void reset()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            val[i][j] = 0;
+            lev[i][j] = INF;
+        }
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        
+        cin >> n >> m;
+        reset();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> val[i][j];
+            }
+        }
+        cout << bfs() << endl;
+    }
+    return 0;
+}
